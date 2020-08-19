65bd3cfffc3df493ca68effe132d13a27f47c838
1234567890Joe <absolutedestroy@gmail.com>
2020-08-20 00:36:58 +0900

    add comment and remove unuse code

--- app.py
+++ app.py
@@ -15,36 +15,28 @@ slack_events_adapter = SlackEventAdapter(
 # Initialize a Web API client
 slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
-# For simplicity we'll store our app data in-memory with the following data structure.
-# onboarding_tutorials_sent = {"channel": {"user_id": OnboardingTutorial}}
-onboarding_tutorials_sent = {}
+# Make BOT Users List
 BOTS = []
 for i in slack_web_client.users_list().get("members"):
     if i.get("is_bot") is True:
         BOTS.append(i.get("id"))
 
-# ============== Message Events ============= #
-# When a user sends a DM, the event type will be 'message'.
-# Here we'll link the message callback to the 'message' event.
+
 @slack_events_adapter.on("message")
 def message(payload):
-    """Display the onboarding welcome message after receiving a message
-    that contains "start".
-    """
     event = payload.get("event", {})
 
     channel_id = event.get("channel")
     user_id = event.get("user")
-    if user_id in BOTS:
-        return 0
-    text = event.get("text")
-
-    haikus = n575_detector(text)
-    if len(haikus) != 0:
-        for i in haikus:
-            slack_web_client.chat_postMessage(
-                channel=channel_id, text=i
-            )
+    print(BOTS, user_id)
+    if user_id not in BOTS:
+        text = event.get("text")
+        haikus = n575_detector(text)
+        if len(haikus) != 0:
+            for i in haikus:
+                slack_web_client.chat_postMessage(
+                    channel=channel_id, text=i
+                )
 
 
 if __name__ == "__main__":
